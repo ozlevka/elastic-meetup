79568c213277b01773fd3d222092caf7adde9b0b
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-10 21:37:02 +0300

    Init project

---
+++ gcloud.sh
@@ -0,0 +1,6 @@
+gcloud compute --project "ozlevka" instances create "docker-test" --zone "us-east4-c" --machine-type "n1-standard-4" --network "default" \
+      --metadata "startup-script=mkdir -p /home/ozlevka/mydisk && chown ozlevka:ozlevka /home/ozlevka/mydisk && mount -t ext4 /dev/sdb1 /home/ozlevka/mydisk && exec /home/ozlevka/mydisk/meetup/install-docker" \
+      --no-restart-on-failure --maintenance-policy "TERMINATE" --preemptible --service-account "228176938798-compute@developer.gserviceaccount.com" \
+       --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+       --disk "name=docker-disk,device-name=docker-disk,mode=rw,boot=no" --image "ubuntu-1604-xenial-v20171002" --image-project "ubuntu-os-cloud" \
+       --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "docker-test"
---
+++ startup.sh
@@ -0,0 +1,3 @@
+mkdir -p /home/ozlevka/meetup
+chown ozlevka:ozlevka /home/ozlevka/meetup
+mount -t ext4 /dev/sdb1 /home/ozlevka/meetup
