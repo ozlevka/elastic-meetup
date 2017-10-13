1e8f2c3a0ead70a46cf90cbc1c10aba7e42cf721
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-13 11:27:02 +0300

    Append var for new options

--- Debian/6.0.0/entrypoint.sh
+++ Debian/6.0.0/entrypoint.sh
@@ -1,2 +1,2 @@
 
-su - elastic -s /bin/sh -c '/opt/elasticsearch/bin/elasticsearch --E cluster.name=test'
+su - elastic -s /bin/sh -c "/opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
