62a8925f874d922d8c6bb43ae8065f90dd035ad4
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-20 18:44:52 +0300

    Append JAVA_OPTS

--- Alpine/5.6.3/entrypoint.sh
+++ Alpine/5.6.3/entrypoint.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
 
-su - elastic -s /bin/sh -c "/opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
+if [ -z "ADD_JAVA_OPTS" ]; then
+    su - elastic -s /bin/sh -c "/opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
+else
+    su - elastic -s /bin/sh -c "ES_JAVA_OPTS=$ADD_JAVA_OPTS && /opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
+fi
--- demotest/main.py
+++ demotest/main.py
@@ -1,6 +1,8 @@
 import json
 from google.cloud import storage
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers as blk
+import os
 
 
 
@@ -23,7 +25,7 @@ storage_client = storage.Client.from_service_account_json("./Enigmatos-468c9c05e
 bucket = storage_client.get_bucket(bucket_name)
 
 
-blobs = bucket.list_blobs(prefix="meetup/9d393db7")
+blobs = bucket.list_blobs(prefix=os.environ["BUCKET"])
 
 counter = 0
 bulk = []
@@ -31,19 +33,24 @@ for blob in blobs:
     counter = counter + 1
     b_data = bucket.get_blob(blob.name)
     tmp = b_data.download_as_string()
+    struct = None
     try:  # try parsing to dict
         struct = json.loads(tmp)
-        obj = make_bulk_object(struct)
-        res = es_client.index('enigmatos', doc_type='test', body=struct)
     except Exception as ex:
         print(ex)
         print("Error in file: {}".format(blob.name))
-    else:
-        print(res)
 
-    # if len(bulk) >= es_bulk_count:
-    #     print(es_client.bulk(bulk))
-    #     bulk = []
+    if not struct is None:
+        obj = make_bulk_object(struct)
+        bulk.append(obj)
+        #res = es_client.index('enigmatos', doc_type='test', body=struct)
+
+    if len(bulk) >= es_bulk_count:
+        print(blk.bulk(es_client, bulk))
+        bulk = []
+
+if len(bulk) > 0:
+    print(blk.bulk(es_client, bulk))
 
 
 print('Total count: {}'.format(counter))
