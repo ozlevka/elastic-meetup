f91d262f50e6f6b03c45a712f65e9526c84dcd78
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-20 19:35:47 +0300

    Fix heap size for all scripts

--- Alpine/2.4.6/entrypoint.sh
+++ Alpine/2.4.6/entrypoint.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
 
-su - elastic -s /bin/sh -c "/opt/elasticsearch/bin/elasticsearch -Dcluster.name=test $ELASTIC_CMD_OPTIONS"
+if [ -z "$ES_HEAP_SIZE" ]; then
+    su - elastic -s /bin/sh -c "/opt/elasticsearch/bin/elasticsearch -Dcluster.name=test $ELASTIC_CMD_OPTIONS"
+else
+    su - elastic -s /bin/sh -c "export ES_HEAP_SIZE=$ES_HEAP_SIZE && /opt/elasticsearch/bin/elasticsearch -Dcluster.name=test $ELASTIC_CMD_OPTIONS"
+fi
