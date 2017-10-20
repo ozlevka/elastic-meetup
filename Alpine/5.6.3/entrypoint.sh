d5e7f700c1c1b42d670036b8061510e434525222
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-20 18:48:56 +0300

    Fix script

--- Alpine/5.6.3/entrypoint.sh
+++ Alpine/5.6.3/entrypoint.sh
@@ -3,5 +3,5 @@
 if [ -z "ADD_JAVA_OPTS" ]; then
     su - elastic -s /bin/sh -c "/opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
 else
-    su - elastic -s /bin/sh -c "ES_JAVA_OPTS=$ADD_JAVA_OPTS && /opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
+    su - elastic -s /bin/sh -c "ES_JAVA_OPTS=\"$ADD_JAVA_OPTS\" && /opt/elasticsearch/bin/elasticsearch --E cluster.name=test $ELASTIC_CMD_OPTIONS"
 fi
